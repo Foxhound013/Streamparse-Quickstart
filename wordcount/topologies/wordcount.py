cad56ea089bf26920e6f9c32c647883f02e54828
Foxhound013 <downingstech@gmail.com>
2018-11-29 12:21:34 -0500

    Minor update
    Minor update to test commit.

--- wordcount/topologies/wordcount.py
+++ wordcount/topologies/wordcount.py
@@ -9,5 +9,5 @@ from spouts.words import WordSpout
 
 
 class WordCount(Topology):
-    word_spout = WordSpout.spec()
+    word_spout = WordSpout.spec() # spec sets the specification of a spout or bolt. can take a name, par, and config argument.
     count_bolt = WordCountBolt.spec(inputs={word_spout: Grouping.fields('word')}, par=2) # par refers to the number of parallel processes for that operation.
